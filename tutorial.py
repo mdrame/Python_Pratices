53ce8e16c66bfc8de1847c7fbfab808d806a2284
mohammed <mdrame113@gmail.com>
2019-11-19 15:14:27 -0800

    Understand slicing with string and list

--- tutorial.py
+++ tutorial.py
@@ -1,52 +1,15 @@
-# understanding class and object 
+
+# Slicing 
+# String and list manupulation
+
+familyMember = ['Mohammed', 'Jenneh', 'About', 'Mariaame', 'Oumou', 'Amara', 'Aly', 'Aisata', 'Lamine']
+newArray = list()
+#print last index of the list
+# print(familyMember[-1])
 
 
-#my dad class
-class Dad():
-    # constructor method
-    def __init__(self, last_name, race, height, date_of_birth):
-        self.last_name = last_name
-        self.race = race
-        self.height = height
-        self.date_of_birth = date_of_birth
-
-# Me class inheriting from Dad class.
-class Me(Dad):
-    # constructor method
-    def __init__(self, last_name, race, height, date_of_birth, sex ):
-        # super.__init__ so we don't got to call self. on ever parameter
-        super().__init__(last_name, race, height, date_of_birth)
-        self.sex = sex
-
-    def walking(self):
-        return "Walking"
-
-my_dad = Dad("Amara", "Black", "5-10", 1924)
-
-me = Me(my_dad.last_name, my_dad.race,  6, 1950, "Male")
-
-
-print(f"\n{my_dad}\n")
-print("Dad:\n\n\n\n\n\n\n")
-
-print("\n _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n")
-
-print("Me:")
-print(f"\n{me.walking()}\n\n\n\n\n\n\n\n\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#get ever name that start with m in this array
+print(familyMember[0:-1:2])
 
 
 
